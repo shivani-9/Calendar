2a66c24732808284e37e5ff61c156760c320b7fb
shivani-9 <shivanidubey634@gmail.com>
2020-06-04 00:23:12 +0530

    first commit

---
+++ app/src/main/java/com/example/clndr/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.clndr
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.DatePicker
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        pickDateBtn.setOnClickListener{
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{ view: DatePicker?, year: Int, month: Int, dayOfMonth: Int ->
+                dateTv.setText("" + dayOfMonth + "/" + month + "/" + year)  },
+                    year,month,day)
+            dpd.show()
+        }
+
+    }
+}
